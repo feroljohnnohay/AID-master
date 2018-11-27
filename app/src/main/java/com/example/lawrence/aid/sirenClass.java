b799881e6ee884b71e1a4966ecd42819d0c2a8b1
Ferol John Nohay <2015081392@ust-ics.mygbiz.com>
2018-11-27 22:12:09 +0800

    modified

--- app/src/main/java/com/example/lawrence/aid/sirenClass.java
+++ app/src/main/java/com/example/lawrence/aid/sirenClass.java
@@ -1,18 +1,33 @@
 package com.example.lawrence.aid;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
 public class sirenClass extends AppCompatActivity {
+
+    MediaPlayer mediaPlayer;
+    Button btnSiren;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.siren);
     }
 
+
+
+    public void startSiren(View v){
+        mediaPlayer = MediaPlayer.create(this, R.raw.siren);
+        mediaPlayer.start();
+        mediaPlayer.setLooping(true);
+    }
+
     public void backButtonSi(View v){
+        mediaPlayer.stop();
         Intent i = new Intent(this, MenuClass.class);
         startActivity(i);
     }
