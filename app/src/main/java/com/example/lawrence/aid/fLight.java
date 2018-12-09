ff237b7200c433669773b6fead5e9a3b1cfbe561
Ferol John Nohay <2015081392@ust-ics.mygbiz.com>
2018-12-10 01:59:43 +0800

    flashlight: working

--- app/src/main/java/com/example/lawrence/aid/fLight.java
+++ app/src/main/java/com/example/lawrence/aid/fLight.java
@@ -1,20 +1,26 @@
 package com.example.lawrence.aid;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Camera;
 import android.hardware.camera2.CameraManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Switch;
 
 public class fLight extends AppCompatActivity {
 
     //For the changing of UI.....//
-    private CameraManager CM_Manager;
-    private String CM_Id;
-    private ImageButton Switch;
-    private Boolean was_LightsOn;
+    android.hardware.Camera camera;
+    android.hardware.Camera.Parameters parameters;
+    ImageButton Switch;
+    private Boolean has_flash = false;
+    private Boolean was_LightsOn = false;
     //..........................//
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,62 +28,49 @@ public class fLight extends AppCompatActivity {
         setContentView(R.layout.flashlight);
 
         Switch = (ImageButton) findViewById(R.id.lightOff);
-        was_LightsOn = false;
+        if(getApplicationContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)){
+            camera = android.hardware.Camera.open();
+            parameters = camera.getParameters();
+            has_flash = true;
+        }
 
-        Switch.setOnClickListener(new View.OnClickListener() {
+        Switch.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View v) {
-                try {
-                    if (was_LightsOn){
-                        turnOff();
-                        was_LightsOn = false;
-                    } else{
-                        turnOn();
+            public void onClick(View v){
+                if (has_flash){
+                    if (was_LightsOn == false){
+                        Switch.setImageResource(R.drawable.on);
+                        parameters.setFlashMode(android.hardware.Camera.Parameters.FLASH_MODE_TORCH);
+                        camera.setParameters(parameters);
+                        camera.startPreview();
                         was_LightsOn = true;
+                    } else {
+                        Switch.setImageResource(R.drawable.off);
+                        parameters.setFlashMode(android.hardware.Camera.Parameters.FLASH_MODE_OFF);
+                        camera.setParameters(parameters);
+                        camera.stopPreview();
+                        was_LightsOn = false;
                     }
-                }catch (Exception e){
-                    e.printStackTrace();
+
+
+                } else{
+                    AlertDialog.Builder builder = new AlertDialog.Builder(fLight.this);
+                    builder.setTitle("Error...");
+                    builder.setMessage("Flashlight is not Available on this Device");
+                    builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                            finish();
+                        }
+                    });
+                    AlertDialog alertDialog = builder.create();
+                    alertDialog.show();
                 }
             }
         });
     }
 
-    @Override
-    protected void onStop(){
-        super.onStop();
-    }
-
-    @Override
-    protected void onResume(){
-        super.onResume();
-    }
-
-    @Override
-    protected void onPause(){
-        super.onPause();
-    }
-
-    public void turnOn(){
-        try {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-                CM_Manager.setTorchMode(CM_Id, true);
-                Switch.setImageResource(R.drawable.on);
-            }
-        } catch (Exception e){
-            e.printStackTrace();
-        }
-    }
-
-    public void turnOff(){
-        try {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-                CM_Manager.setTorchMode(CM_Id, false);
-                Switch.setImageResource(R.drawable.off);
-            }
-        } catch (Exception e){
-            e.printStackTrace();
-        }
-    }
 
     public void backButtonFlashlight(View v){
         Intent i = new Intent(this, MenuClass.class);
